d59437dfd2ed222107d879ad8f91198c05fec9dc
Yann <yann24.bonnau@hotmail.fr>
2022-11-29 21:29:38 +0100

    Ajout gh actions et premier test (#11)

---
+++ application/src/test/java/com/mineaurion/api/query/QueryControllerTest.java
@@ -0,0 +1,54 @@
+package com.mineaurion.api.query;
+
+import com.mineaurion.api.library.model.query.Access;
+import com.mineaurion.api.library.model.query.Schedule;
+import com.mineaurion.api.library.model.query.Server;
+import com.mineaurion.api.library.model.query.Version;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = QueryController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class QueryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private QueryService queryService;
+
+    Server mockServer = new Server(
+            "Test Name",
+            new Version("1.0.0", "1.0.0"),
+            "skyblock",
+            new Access(false, false, false),
+            "dns.example.com",
+            new Schedule(Arrays.asList("09h00", "10h00"))
+    );
+
+    @Test
+    public void getAllQueryWithoutQueryParams() throws Exception {
+        Mockito.when(queryService.findAll(Sort.Direction.ASC, "id")).thenReturn(Arrays.asList(mockServer));
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/query").accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        Mockito.verify(queryService, Mockito.times(1)).findAll(Sort.Direction.ASC, "id");
+    }
+
+}
